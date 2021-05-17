540b6021ece20849085e891d80741dd6e92ed7fe
Łukasz Nycz <lnycz96@gmail.com>
2021-05-17 17:40:17 +0200

    form labels and send print

--- ReservationForm/ReservationForm.swift
+++ ReservationForm/ReservationForm.swift
@@ -10,18 +10,44 @@ import UIKit
 protocol ReservationFormDelegate {
     func didTapButton(model: TableModel)
 }
+
+struct userModel {
+    let userName: String
+    let userSurname: String
+    let userPhone: String
+    let userReservation: Date
+}
 class ReservationForm: UIViewController {
     var reservationFormDelegate: ReservationFormDelegate!
     var model: TableModel?
+    var modelUser:userModel?
+    
+    @IBOutlet weak var userNameLabel: UITextField!
+    @IBOutlet weak var userSurnameLabel: UITextField!
+    @IBOutlet weak var userPhoneLabel: UITextField!
+    @IBOutlet weak var userDateReservationLabel: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+    }
+    func userInfo(model: userModel) {
+
+        userNameLabel.text = model.userName
+        userSurnameLabel.text = model.userSurname
+        userPhoneLabel.text = model.userPhone
+        userDateReservationLabel.date = model.userReservation
+        
+        let userInformation = userModel(userName: model.userName, userSurname: model.userSurname, userPhone: model.userPhone, userReservation: model.userReservation)
+        print(userInformation)
     }
     
     @IBAction func reserveButton(_ sender: Any) {
         guard let model = model else { return }
         dismiss(animated: true, completion: nil)
         reservationFormDelegate.didTapButton(model: model)
+       
+        userInfo(model: modelUser!)
     }
     @IBAction func backToRestaurantButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
