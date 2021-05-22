ba756aee091da4678d5d12305fc400dcc6f5cc0a
Łukasz Nycz <lnycz96@gmail.com>
2021-05-22 12:20:59 +0200

    Creating Menu storyboard and file

---
+++ MenuVC.swift
@@ -0,0 +1,18 @@
+//
+//  MenuVC.swift
+//  PubBar
+//
+//  Created by Łukasz Nycz on 22/05/2021.
+//
+
+import UIKit
+
+class MenuVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
