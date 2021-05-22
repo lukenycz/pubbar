a767c95de8346a245c00367748315d0af6372d08
Łukasz Nycz <lnycz96@gmail.com>
2021-05-22 12:29:09 +0200

    Json Parsing

--- MenuVC.swift
+++ MenuVC.swift
@@ -11,8 +11,41 @@ class MenuVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        parseJson()
 
 
     }
+    private func parseJson() {
+        guard let path = Bundle.main.path(forResource: "PubBarMenu", ofType: "json") else{
+            return
+        }
+        
+        let url = URL(fileURLWithPath: path)
+        var result:Result?
+        
+        do {
+        let jsonData = try Data(contentsOf: url)
+            result = try JSONDecoder().decode(Result.self, from: jsonData)
+            
+            if let result = result {
+                print(result)
+            } else {
+                print("Failed to pase")
+            }
+            return
+        } catch {
+            print("Error: \(error)")
+        }
+    }
 
+}
+
+struct Result:Codable {
+    let data:[ResultItem]
+}
+
+struct ResultItem:Codable {
+    let name: String
+    let description: String
+    let price: Int
 }
