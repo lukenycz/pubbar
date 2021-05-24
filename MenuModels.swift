c9b40a6db1de5a1ffba04d6b7124e08797598da5
Łukasz Nycz <lnycz96@gmail.com>
2021-05-24 14:30:49 +0200

    json update

---
+++ MenuModels.swift
@@ -0,0 +1,40 @@
+//
+//  MenuModels.swift
+//  PubBar
+//
+//  Created by Łukasz Nycz on 23/05/2021.
+//
+
+import Foundation
+
+struct Menu:Codable {
+    let menu:[MenuContains]
+}
+
+struct MenuContains:Codable {
+    let type:String?
+    let drinks:[Drinks]
+    let softdrinks2:[SoftDrinks]
+    let snacks:[Snacks]
+    let pizza:[Pizza]
+}
+struct Drinks:Codable {
+    let name: String
+    let description: String
+    let price: Int
+}
+struct SoftDrinks:Codable {
+    let name: String
+    let description: String
+    let price: Int
+}
+struct Snacks:Codable {
+    let name: String
+    let description: String
+    let price: Int
+}
+struct Pizza:Codable {
+    let name: String
+    let description: String
+    let price: Int
+}
--- MenuVC.swift
+++ MenuVC.swift
@@ -7,32 +7,76 @@
 
 import UIKit
 
-class MenuVC: UIViewController {
+class MenuVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var menu:Menu?
+    
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero,
+                                style: .grouped)
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         parseJson()
-
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        tableView.delegate = self
+        tableView.dataSource = self
 
     }
+    // TableView
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return menu?.menu.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return menu?.menu[section].drinks.description
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let menu = menu {
+           return menu.menu[section].drinks.count
+           // menu.menu![section].softdrinks.count
+           // menu.menu![section].snacks.count
+           // menu.menu![section].pizza.count
+        }
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let name = menu?.menu[indexPath.section].drinks[indexPath.item].name
+        let description = menu?.menu[indexPath.section].drinks[indexPath.item].description
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = name
+        cell.detailTextLabel?.text = description
+        return cell
+    }
+    
+    // JSON
     private func parseJson() {
-        guard let path = Bundle.main.path(forResource: "PubBarMenu", ofType: "json") else{
+        guard let path = Bundle.main.path(
+                forResource: "PubBarMenu",
+                ofType: "json"
+        ) else {
             return
         }
         
         let url = URL(fileURLWithPath: path)
-        var menu:Menu?
         
         do {
         let jsonData = try Data(contentsOf: url)
-            menu = try JSONDecoder().decode(Menu.self, from: jsonData)
-            
+            menu = try JSONDecoder().decode(Menu.self, from: jsonData)            
+         /*  //
             if let menu = menu {
                 print(menu)
             } else {
                 print("Failed to pase")
             }
             return
+           */ //
         } catch {
             print("Error: \(error)")
         }
@@ -40,33 +84,4 @@ class MenuVC: UIViewController {
 
 }
 
-struct Menu:Codable {
-    let menu:[MenuContains]?
-}
 
-struct MenuContains:Codable {
-    let drinks:[Drinks]
-    let softdrinks:[SoftDrinks]
-    let snacks:[Snacks]
-    let pizza:[Pizza]
-}
-struct Drinks:Codable {
-    let name: String
-    let description: String
-    let price: Int
-}
-struct SoftDrinks:Codable {
-    let name: String
-    let description: String
-    let price: Int
-}
-struct Snacks:Codable {
-    let name: String
-    let description: String
-    let price: Int
-}
-struct Pizza:Codable {
-    let name: String
-    let description: String
-    let price: Int
-}
